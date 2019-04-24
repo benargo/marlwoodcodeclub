b5b926c134f948fede6f17bcce02c1af0fa1a98f
Ben Argo <ben@benargo.com>
2019-04-24 15:43:00 +0100

    Fix card grids

--- resources/views/themes/marlwood/blocks/repeaters/cards.blade.php
+++ resources/views/themes/marlwood/blocks/repeaters/cards.blade.php
@@ -1,8 +1,8 @@
 @if ($is_first)
 <div class="row">
-    <div class="col col-sm-4">
 @endif
 
+<div class="col col-sm-4">
     <a href="{{ PageBuilder::block('link') }}" class="card">
         <div class="card-header">
             <h5 class="card-title">{{ PageBuilder::block('title') }}</h5>
@@ -10,12 +10,15 @@
         @if (PageBuilder::block('image'))
             {!! PageBuilder::block('image', ['class' => 'card-img-top']) !!}
         @endif
-        <div class="card-body">
-            {!! PageBuilder::block('content') !!}
-        </div>
+        @if (PageBuilder::block('content'))
+            <div class="card-body">
+                {!! PageBuilder::block('content') !!}
+            </div>
+        @endif
     </a>
+</div>
 
 @if ($is_last)
-    </div>
+
 </div>
 @endif
